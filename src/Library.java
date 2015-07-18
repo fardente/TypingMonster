516a9aa40e928aa3ddb224235ed3fdfe5a1aaeb3
Flashbob <flashbobthundermore@gmail.com>
2015-07-18 13:02:06 +0200

    Cleaned code, added comments, check for special chars

--- src/Library.java
+++ src/Library.java
@@ -1,50 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 /**
  *
  * @author G510
  */
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.regex.Pattern;
+
 public class Library {
 
     /**
      * @param args the command line arguments
      */
-    private ArrayList<String> word=new ArrayList<String>();
-    private String tosend[]=new String [20];
-    private int total=128985;
-    private ArrayList<String> alreadysend=new ArrayList<String>();
+    private ArrayList<String> word;      //ArrayList of words
+    private String tosend[];            //array of words to send  
+    private int total;                 //total number of words in a file
+    int randomInt;                    //random number  
+    Random randomGenerator;          //random number generator
+    
+    
+    /**
+     * sets total to 0
+     * calls loadfile() method
+     */
     Library()
     {
-        loadfile();
+        total=0;
+        randomGenerator = new Random();
+        word=new ArrayList<String>();
+        loadfile();        
     }
+    /**
+     * reads the library.txt file and loads the words in word ArrayList and update total
+     */
     public void loadfile()
     {
-        //read the whole file and store the sentences in the arraylist 
+       //read the whole file and store the sentences in the arraylist 
       //only the sentences with also the words #BOS and #EOS
-      String file="res/library.txt";
-     String line;
-     int count=0;
-     //String[] words=new String[8];
-     String words; 
-     BufferedReader src1;
-     String encoding="UTF-8";
-     int fre;
-    // BufferedReader src2;
-     //NameFrequency obj;
-  //blist=new NoRepeatsList();
-  //glist=new NoRepeatsList();
-  try 
-  {
-    src1 = new BufferedReader(new InputStreamReader(new FileInputStream(file),"UTF-8"));
-    while ((line = src1.readLine()) != null)
-    {
+        String file="res/library.txt";
+        String line;
+        String words;
+        BufferedReader src1;
+        Pattern p = Pattern.compile("[^a-z]");
+
+     try 
+     {
+      src1 = new BufferedReader(new InputStreamReader(new FileInputStream(file),"UTF-8"));
+      while ((line = src1.readLine()) != null)
+      {
           words = line;
-          word.add(words);
+          if (!p.matcher(words).find()){
+              word.add(words);
+              total++;
+          }
+      }
+      src1.close();
      }
-     src1.close();
-  }
-    catch (FileNotFoundException e)
+        catch (FileNotFoundException e)
         {
             System.out.println(e.getMessage());
             System.exit(0);        
@@ -60,76 +78,42 @@ public class Library {
             System.exit(0);
         }
     }
-    public String[] get(int length)
-    {
-        ArrayList<Integer> arr=new ArrayList<Integer>();
-        Random randomGenerator = new Random();
-       
-    for (int i = 0; i < 20; ){
-      int randomInt = randomGenerator.nextInt(total);
-      if(!arr.contains(randomInt) && (word.get(randomInt).length()==length))
-      {
-          arr.add(randomInt);
-          tosend[i]=word.get(randomInt);
-          i++;
-      }
-    }
-    int a;
-    for (int j=0;j<20;j++)
-    {
-        alreadysend.add(word.get(j));
-        a=arr.get(j);
-        word.remove(a);
-    }
-    total=total-20;
-    return tosend;
-    }
-    public void returned(String array[])
-    {
-        for(int i=0;i<20;i++)
-        {
-            if(!array[i].equals(""))
-            {
-                word.add(array[i]);
-                total++;
-            }
-        }
-    }
-//----------------------------------------------------------------------------------
+    /**
+     * This method generates the random integers and the checks if length of word at given index= lenght of word
+     * saves it in tosend array and returns
+     * @param length     length of the word
+     * @param numwords   number of words
+     * @return 
+     */
+
      public String[] get(int length,int numwords)
     {
-        ArrayList<Integer> arr=new ArrayList<Integer>();
-        Random randomGenerator = new Random();
-        String []send=new String[numwords];       
-    for (int i = 0; i < numwords; ){
-      int randomInt = randomGenerator.nextInt(total);
-      if(!arr.contains(randomInt) && (word.get(randomInt).length()==length))
-      {
-          arr.add(randomInt);
-          send[i]=word.get(randomInt);
-          i++;
-      }
-    }
-    int a;
-    for (int j=0;j<20;j++)
-    {
-        alreadysend.add(word.get(j));
-        a=arr.get(j);
-        word.remove(a);
-    }
-    total=total-numwords;
-    return send;
+        ArrayList<Integer> arr=new ArrayList<Integer>();   //check if index is not already generated
+        tosend=new String[numwords];       
+        for (int i = 0; i < numwords; )
+        {
+            randomInt = randomGenerator.nextInt(total);
+            if(!arr.contains(randomInt) && (word.get(randomInt).length()==length))
+            {
+             arr.add(randomInt);
+             tosend[i]=word.get(randomInt);
+             i++;
+            }
+        }
+    return tosend;
     }
     public static void main(String[] args) {
         // TODO code application logic here
          Library l=new Library();
          String []a=new String[1000];
+         System.out.println(l.total);
          a=l.get(4,1000);
          
          for(int i=0;i<1000;i++)
          {
              System.out.println(a[i]);
          }
+         System.out.println(l.total);
     }
     
 }
